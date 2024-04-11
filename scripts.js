087272a5136abfa6de3500137d4c98a5a77f3d0d
Sajid <sajidmohammad460519@gmail.com>
2024-04-12 02:16:56 +0800

    made html file less cluttered, and maked website ui look better

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Get the link elements
+const whyMeLink = document.getElementById('why-me-link');
+const angels1Link = document.getElementById('angels1-link');
+
+// Set the href attribute for each link
+whyMeLink.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa';
+angels1Link.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc';
