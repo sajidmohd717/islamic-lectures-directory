4ce322f39a35052fd9c56f706abb0b6fb92edf06
Sajid <sajidmohammad460519@gmail.com>
2024-04-16 23:46:04 +0800

    added more playlists and added a new section called life of the prophet, also fixed some bugs

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 // Get the link elements
 const whyMeLink = document.getElementById('why-me-link');
 const angels1Link = document.getElementById('angels1-link');
+const quran30 = document.getElementById('quran-30-lessons-link')
+const parablesQuran = document.getElementById('parables-quran-link')
 
 // Set the href attribute for each link
 whyMeLink.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa';
 angels1Link.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc';
+quran30.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUnn09Of4rBuakQhu-Q2qpc&si=uYzvoE9Buq4NTwKH'
+parablesQuran.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUIsBRE5XCY6eICwl_W8jnj&si=ycQJB_M7bKswa1Se'
