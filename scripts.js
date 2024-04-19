ebefa95cdb78b26ca9ad6a4523d76d6ac97f9d51
Sajid <sajidmohammad460519@gmail.com>
2024-04-20 01:10:54 +0800

    added no of episodes, and fixed link for one of the videos

--- scripts.js
+++ scripts.js
@@ -1,20 +1,33 @@
 // Get the link elements
 
 // Heart Softeners
+
 const whyMeLink = document.getElementById('why-me-link');
 const angels1Link = document.getElementById('angels1-link');
 
+whyMeLink.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa';
+angels1Link.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc';
+
 // General Quran Tafsir
+
 const quran30 = document.getElementById('quran-30-lessons-link')
 const parablesQuran = document.getElementById('parables-quran-link')
 
-// Lift of the Prophet
-const seerahYQ = document.getElementById('seerah-qadhi-link')
-
-
-// Set the href attribute for each link
-whyMeLink.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa';
-angels1Link.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc';
 quran30.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUnn09Of4rBuakQhu-Q2qpc&si=uYzvoE9Buq4NTwKH'
 parablesQuran.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUIsBRE5XCY6eICwl_W8jnj&si=ycQJB_M7bKswa1Se'
+
+
+// Lift of the Prophet
+
+const seerahYQ = document.getElementById('seerah-qadhi-link')
+
 seerahYQ.href = 'https://www.youtube.com/playlist?list=PLAEA99D24CA2F9A8F'
+
+// Learn Arabic
+const medinaBook = document.getElementById('medina-book-link')
+
+medinaBook.href = 'https://www.youtube.com/playlist?list=PLh3c1SPUmZqGMOtrHD7Kd2uXuvPRDP7ou'
+
+
+
+
