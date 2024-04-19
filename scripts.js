3df3afae17d0e3c5c00876a98097a8846ff2abd7
Sajid <sajidmohammad460519@gmail.com>
2024-04-20 00:35:56 +0800

    designed website for mobile, added new videos and sections

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 // Get the link elements
+
+// Heart Softeners
 const whyMeLink = document.getElementById('why-me-link');
 const angels1Link = document.getElementById('angels1-link');
+
+// General Quran Tafsir
 const quran30 = document.getElementById('quran-30-lessons-link')
 const parablesQuran = document.getElementById('parables-quran-link')
+
+// Lift of the Prophet
+const seerahYQ = document.getElementById('seerah-qadhi-link')
+
 
 // Set the href attribute for each link
 whyMeLink.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa';
 angels1Link.href = 'https://youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc';
 quran30.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUnn09Of4rBuakQhu-Q2qpc&si=uYzvoE9Buq4NTwKH'
 parablesQuran.href = 'https://youtube.com/playlist?list=PLYZxc42QNctUIsBRE5XCY6eICwl_W8jnj&si=ycQJB_M7bKswa1Se'
+seerahYQ.href = 'https://www.youtube.com/playlist?list=PLAEA99D24CA2F9A8F'
