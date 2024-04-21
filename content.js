dc92cac9f0d94c008e7e5d870be7f73892d201bc
Sajid <sajidmohammad460519@gmail.com>
2024-04-21 15:33:39 +0800

    added content.js, planning to remove code from html to js file

---
+++ content.js
@@ -0,0 +1,15 @@
+// Wow, the HTML file is getting huge and it's going to be hard to add content to it by just typing, i'll try to refactor the code and try to have some code be in a javascript file
+
+// My plan is to have a list of dictionaries maybe something like this
+
+/*
+
+let heartSofteners = [
+    {Name: "Why Me?", Author: "Omar Suleiman", NoEpisodes: "32 Episodes"},
+    {Name: "Angels in Your Presence", Author: "Omar Suleiman", NoEpisodes: "32 Episodes"},
+]
+
+*/
+
+// If i do it like this i don't have to write so much damn muntaine HTML 
+// :((((((
