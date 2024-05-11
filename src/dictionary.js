5d83c8f518160b956b17c0b11353b41960a7fb99
Sajid <sajidmohammad460519@gmail.com>
2024-05-11 10:43:42 +0800

    made use of es6 modules :D

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,64 +1,3 @@
-// Define a function to create HTML elements for each section
-function createSectionElements(sectionDivTitle, sectionList, sectionTitle) {
-  // Create section header
-  const sectionDiv = document.createElement('div')
-  const contentDiv = document.querySelector('.content')
-  sectionDiv.classList.add(sectionDivTitle)
-  const sectionHeader = document.createElement("h3");
-  sectionHeader.classList.add("section-title");
-  sectionHeader.innerHTML = `<hr>${sectionTitle}`;
-  sectionDiv.appendChild(sectionHeader);
-  contentDiv.appendChild(sectionDiv)
-
-  // Loop through the list and create HTML elements for each item
-  sectionList.forEach((item) => {
-    // Create section div
-    const sectionItemDiv = document.createElement("div");
-    sectionItemDiv.classList.add(`${item.class}`);
-
-    // Create thumbnail div
-    const thumbnailContainer = document.createElement("div");
-    thumbnailContainer.classList.add("thumbnail-container");
-    thumbnailContainer.innerHTML = `<a href="${item.link}"><img class="thumbnail" src="${item.thumbnailImage}" alt=""></a>`;
-
-    // Create title-author div
-    const titleAuthor = document.createElement("div");
-    titleAuthor.classList.add("title-author");
-
-    // Create title div
-    const titleDiv = document.createElement("div");
-    titleDiv.classList.add("title");
-    titleDiv.innerHTML = `<span>${item.title}<br></span>`; // Use item.title from the current dictionary
-
-    // Create meta-title div
-    const metaTitleDiv = document.createElement("div");
-    metaTitleDiv.classList.add("meta-title");
-
-    // Create meta-title-left
-    const metaTitleLeft = document.createElement("div");
-    metaTitleLeft.classList.add("meta-title-left");
-    metaTitleLeft.innerHTML = `<span class="author">${item.speaker}<br></span> <span class="no-videos">${item.episodes}<br></span>`; // Use item.speaker and item.episodes
-
-    // Create meta-title-right
-    const metaTitleRight = document.createElement("div");
-    metaTitleRight.classList.add("meta-title-right");
-    metaTitleRight.innerHTML =
-      '<a href="" class="down-arrow"><img class="down-arrow-svg" src="images/down-arrow.svg" alt=""></a>';
-
-    // Appending child elements
-    metaTitleDiv.appendChild(metaTitleLeft);
-    metaTitleDiv.appendChild(metaTitleRight);
-    titleAuthor.appendChild(titleDiv);
-    titleAuthor.appendChild(metaTitleDiv);
-    sectionItemDiv.appendChild(thumbnailContainer);
-    sectionItemDiv.appendChild(titleAuthor);
-
-    // Finally, append the sectionItemDiv to sectionDiv
-    sectionDiv.appendChild(sectionItemDiv);
-  });
-}
-
-// Define the combined list of dictionaries
 const combinedList = [
   {
     sectionDivTitle: "heart-softeners",
@@ -133,10 +72,10 @@ const combinedList = [
         speaker: "Yasir Qadhi",
         episodes: "78 Episodes",
         thumbnailImage: "images/lives-sahaba.jpg",
-        class: "lives-sahaba-qadhi"
+        class: "lives-sahaba-qadhi",
       },
     ],
-    sectionTitle: "Lives of the Sahaba"
+    sectionTitle: "Lives of the Sahaba",
   },
   {
     sectionDivTitle: "learn-arabic",
@@ -164,11 +103,8 @@ const combinedList = [
         class: "cultivating-character",
       },
     ],
-    sectionTitle: "Manners & Character"
-  }
+    sectionTitle: "Manners & Character",
+  },
 ];
 
-// Loop through the combined list and create HTML elements for each section
-combinedList.forEach(({ sectionDivTitle, sectionList, sectionTitle }) => {
-  createSectionElements(sectionDivTitle, sectionList, sectionTitle);
-});
+export { combinedList }
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import { combinedList } from "./dictionary.js";
+
+// Define a function to create HTML elements for each section
+function createSectionElements(sectionDivTitle, sectionList, sectionTitle) {
+  // Create section header
+  const sectionDiv = document.createElement('div')
+  const contentDiv = document.querySelector('.content')
+  sectionDiv.classList.add(sectionDivTitle)
+  const sectionHeader = document.createElement("h3");
+  sectionHeader.classList.add("section-title");
+  sectionHeader.innerHTML = `<hr>${sectionTitle}`;
+  sectionDiv.appendChild(sectionHeader);
+  contentDiv.appendChild(sectionDiv)
+
+  // Loop through the list and create HTML elements for each item
+  sectionList.forEach((item) => {
+    // Create section div
+    const sectionItemDiv = document.createElement("div");
+    sectionItemDiv.classList.add(`${item.class}`);
+
+    // Create thumbnail div
+    const thumbnailContainer = document.createElement("div");
+    thumbnailContainer.classList.add("thumbnail-container");
+    thumbnailContainer.innerHTML = `<a href="${item.link}"><img class="thumbnail" src="${item.thumbnailImage}" alt=""></a>`;
+
+    // Create title-author div
+    const titleAuthor = document.createElement("div");
+    titleAuthor.classList.add("title-author");
+
+    // Create title div
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("title");
+    titleDiv.innerHTML = `<span>${item.title}<br></span>`; // Use item.title from the current dictionary
+
+    // Create meta-title div
+    const metaTitleDiv = document.createElement("div");
+    metaTitleDiv.classList.add("meta-title");
+
+    // Create meta-title-left
+    const metaTitleLeft = document.createElement("div");
+    metaTitleLeft.classList.add("meta-title-left");
+    metaTitleLeft.innerHTML = `<span class="author">${item.speaker}<br></span> <span class="no-videos">${item.episodes}<br></span>`; // Use item.speaker and item.episodes
+
+    // Create meta-title-right
+    const metaTitleRight = document.createElement("div");
+    metaTitleRight.classList.add("meta-title-right");
+    metaTitleRight.innerHTML =
+      '<a href="" class="down-arrow"><img class="down-arrow-svg" src="images/down-arrow.svg" alt=""></a>';
+
+    // Appending child elements
+    metaTitleDiv.appendChild(metaTitleLeft);
+    metaTitleDiv.appendChild(metaTitleRight);
+    titleAuthor.appendChild(titleDiv);
+    titleAuthor.appendChild(metaTitleDiv);
+    sectionItemDiv.appendChild(thumbnailContainer);
+    sectionItemDiv.appendChild(titleAuthor);
+
+    // Finally, append the sectionItemDiv to sectionDiv
+    sectionDiv.appendChild(sectionItemDiv);
+  });
+}
+
+// Loop through the combined list and create HTML elements for each section
+combinedList.forEach(({ sectionDivTitle, sectionList, sectionTitle }) => {
+  createSectionElements(sectionDivTitle, sectionList, sectionTitle);
+});
